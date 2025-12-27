edf31e9703b53678c2055c757ca15f0cfdd44458
wendietibrias <wendietibrias@gmail.com>
2025-12-27 13:03:43 +0700

    chore: Moora Calculation Logic (WIP)

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const criteriaItemBody = document.getElementById("criteria-item-body");
 const formBtn = document.querySelector(".criteria-form-btn");
 const calculationBtn = document.querySelector(".calculation-btn");
 
+let squareOfItems = {};
 let arrayOfItems = []; /** should be array of object*/
 
 formBtn.addEventListener("click", function (e) {
@@ -42,28 +43,52 @@ formBtn.addEventListener("click", function (e) {
 });
 
 calculationBtn.addEventListener("click", function () {
+  let idxSquareItem = 0;
+
+  Object.keys(arrayOfItems[0]).forEach((key) => {
+    if (
+      typeof arrayOfItems[0][key] === "number" ||
+      !isNaN(arrayOfItems[0][key])
+    ) {
+      squareOfItems[`${key}`] = {
+         values: [],
+         normalizations: [],
+         calculatedResult: 0
+      };
+    }
+
+    idxSquareItem += 1;
+  });
+
+  
+  for(const itemDetail of arrayOfItems) {
+     for(const [key,value] of Object.entries(itemDetail)){
+         if(squareOfItems[key]) {
+           squareOfItems[key].values.push(value);
+         }
+     }
+
+     for(const [key,value] of Object.entries(squareOfItems)){
+        let calculatedResult = 0;
+        squareOfItems[key].calculatedResult = squareOfItems[key].values.map((detailMapped) => {
+          const currentCalculated = Math.sqrt(detailMapped**2);
+          calculatedResult += Math.sqrt(detailMapped**2);
+        })
+
+        squareOfItems[key].calculatedResult = Math.sqrt(calculatedResult);
+        squareOfItems[key].normalizations = squareOfItems[key].values.map((item) => {
+           return item / squareOfItems[key].calculatedResult;
+        });
+     }
+  }
+
+  for(const finalCalculatedItem of squareOfItems){
+     
+  }
+
   /** calculate result */
 
-  let squareRootOfItem = [];
-  let idxRetrieved = 1;
+  squareOfItems = {};
 
-  for (const [index, value] of arrayOfItems.entries()) {
-    if (index > 0) {
-      const findExisting = squareRootOfItem.find(
-        (item) => item.name === `C${index}`
-      );
-      if (!findExisting) {
-        const payloadCurrentItem = {
-          name: `C${index + 1}`,
-          values: [],
-        };
 
-        for (const [key, valueDetail] of value.entries()) {
-          payloadCurrentItem[values];
-        }
-        squareRootOfItem.push(payloadCurrentItem);
-      }
-      idxRetrieved += 1;
-    }
-  }
 });
