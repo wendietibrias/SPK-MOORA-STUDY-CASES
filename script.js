66aad7c15b6f4f629fc8eadf99d47f8223750344
wendietibrias <wendietibrias@gmail.com>
2025-12-27 19:50:42 +0700

    chore: fix calculation

--- script.js
+++ script.js
@@ -6,22 +6,51 @@ const battery = document.querySelector(".battery");
 const price = document.querySelector(".price");
 const processorSpeed = document.querySelector(".processorSpeed");
 
+const laptopWeight = document.querySelector(".laptopWeight");
+const ramWeight = document.querySelector(".ramWeight");
+const batteryWeight = document.querySelector(".batteryWeight");
+const priceWeight = document.querySelector(".priceWeight");
+const processorSpeedWeight = document.querySelector(".processorSpeedWeight");
+
 const criteriaItemBody = document.getElementById("criteria-item-body");
 
 const formBtn = document.querySelector(".criteria-form-btn");
 const calculationBtn = document.querySelector(".calculation-btn");
 
+const tableResultContainer = document.getElementById('table-result-container');
+const resetBtn = document.querySelector('.reset-result-btn');
+
 let squareOfItems = {};
 let arrayOfItems = []; /** should be array of object*/
 
+function displayResult() {
+  if (arrayOfItems.length > 0) {
+    let parsedValueHTML = "";
+    for (let i = 0; i < arrayOfItems.length; i++) {
+      parsedValueHTML += `
+               <tr>
+                 <td>${arrayOfItems[i].laptopName}</td>
+                 <td>${arrayOfItems[i].score}</td>
+                 <td>${(i + 1)}</td>
+               </tr>
+            `;
+    }
+    tableResultContainer.innerHTML = parsedValueHTML;
+  }
+
+}
+
 formBtn.addEventListener("click", function (e) {
+  squareOfItems = {};
+
   arrayOfItems.push({
     laptopName: laptopName.value,
-    price: parseInt(price.value),
-    ram: parseInt(ram.value),
-    battery: parseInt(battery.value),
-    processorSpeed: parseInt(processorSpeed.value),
-    weight: parseInt(weight.value),
+    price: Number(price.value),
+    ram: Number(ram.value),
+    battery: Number(battery.value),
+    processorSpeed: Number(processorSpeed.value),
+    weight: Number(weight.value),
+    score: 0,
   });
 
   let parsedValueHTML = "";
@@ -40,55 +69,126 @@ formBtn.addEventListener("click", function (e) {
   }
 
   criteriaItemBody.innerHTML = parsedValueHTML;
-});
 
-calculationBtn.addEventListener("click", function () {
   let idxSquareItem = 0;
 
   Object.keys(arrayOfItems[0]).forEach((key) => {
+    console.log(key);
     if (
       typeof arrayOfItems[0][key] === "number" ||
       !isNaN(arrayOfItems[0][key])
     ) {
-      squareOfItems[`${key}`] = {
-         values: [],
-         normalizations: [],
-         calculatedResult: 0
-      };
+
+      if (key !== 'score') {
+        squareOfItems[`${key}`] = {
+          values: [],
+          normalizations: [],
+          calculatedResult: 0,
+        };
+
+        if (laptopWeight.className.toLowerCase().includes(key.toLowerCase())) {
+          squareOfItems[`${key}`]['tipe'] = 'cost'
+          squareOfItems[`${key}`]['bobot'] = Number(laptopWeight.value);
+        }
+        if (ramWeight.className.toLowerCase().includes(key.toLowerCase())) {
+          squareOfItems[`${key}`]['tipe'] = 'benefit'
+          squareOfItems[`${key}`]['bobot'] = Number(ramWeight.value);
+
+        }
+        if (batteryWeight.className.toLowerCase().includes(key.toLowerCase())) {
+          squareOfItems[`${key}`]['tipe'] = 'benefit'
+          squareOfItems[`${key}`]['bobot'] = Number(batteryWeight.value);
+
+        }
+        if (processorSpeedWeight.className.toLowerCase().includes(key.toLowerCase())) {
+          squareOfItems[`${key}`]['tipe'] = 'benefit'
+          squareOfItems[`${key}`]['bobot'] = Number(processorSpeedWeight.value);
+
+        }
+        if (priceWeight.className.toLowerCase().includes(key.toLowerCase())) {
+          squareOfItems[`${key}`]['tipe'] = 'cost'
+          squareOfItems[`${key}`]['bobot'] = Number(priceWeight.value);
+
+        }
+      }
+
     }
 
     idxSquareItem += 1;
   });
+});
 
-  
-  for(const itemDetail of arrayOfItems) {
-     for(const [key,value] of Object.entries(itemDetail)){
-         if(squareOfItems[key]) {
-           squareOfItems[key].values.push(value);
-         }
-     }
 
-     for(const [key,value] of Object.entries(squareOfItems)){
-        let calculatedResult = 0;
-        squareOfItems[key].calculatedResult = squareOfItems[key].values.map((detailMapped) => {
-          const currentCalculated = Math.sqrt(detailMapped**2);
-          calculatedResult += Math.sqrt(detailMapped**2);
+calculationBtn.addEventListener("click", function () {
+
+  for (const itemDetail of arrayOfItems) {
+    for (const [key, value] of Object.entries(itemDetail)) {
+      if (squareOfItems[key] && key !== 'score') {
+        squareOfItems[key].values.push({
+          value: value,
+          group: itemDetail.laptopName,
         });
+      }
+    }
 
-        squareOfItems[key].calculatedResult = Math.sqrt(calculatedResult);
-        squareOfItems[key].normalizations = squareOfItems[key].values.map((item) => {
-           return item / squareOfItems[key].calculatedResult;
-        });
-     }
+    for (const [key, value] of Object.entries(squareOfItems)) {
+      let calculatedResult = 0;
+      squareOfItems[key].calculatedResult = squareOfItems[key].values.map((detailMapped) => {
+        const currentCalculated = Math.sqrt(detailMapped.value ** 2);
+        calculatedResult += currentCalculated;
+      });
+
+      squareOfItems[key].calculatedResult = Math.sqrt(calculatedResult);
+      squareOfItems[key].normalizations = squareOfItems[key].values.map((item) => {
+        return {
+          laptopName: item.group,
+          value: item.value / squareOfItems[key].calculatedResult
+        };
+      });
+    }
   }
 
-  for(const finalCalculatedItem of squareOfItems){
-     
-  }
 
   /** calculate result */
 
-  squareOfItems = {};
+  for (const arrayItem of arrayOfItems) {
+    let calculatedScore = 0;
+    for (const [key, value] of Object.entries(squareOfItems)) {
+      const findByLaptopName = squareOfItems[key].normalizations.find((item) => item.laptopName.toLowerCase().includes(arrayItem.laptopName));
+      if (findByLaptopName) {
+        if (squareOfItems[key].tipe === "cost") {
+          calculatedScore -= Number((findByLaptopName.value * squareOfItems[key].bobot));
+        } else {
+          calculatedScore += Number((findByLaptopName.value * squareOfItems[key].bobot));
+        }
+      }
+    }
+
+    console.log(calculatedScore);
+    arrayItem.score = Math.abs(calculatedScore);
+
+  }
+  arrayOfItems = arrayOfItems.sort((a, b) => b.score - a.score);
+
+  displayResult();
+});
 
 
+resetBtn.addEventListener('click', function(){
+  laptopName.value = null;
+  processorSpeed.value= null;
+  processorSpeedWeight.value = null;
+  ram.value = null
+  ramWeight.value = null;
+  battery.value = null;
+  batteryWeight.value = null;
+  price.value = null;
+  priceWeight.value = null;
+  weight.value  = null;
+  laptopWeight.value = null;
+
+  arrayOfItems = null;
+  tableResultContainer.innerHTML = "";
+
+  tableResultContainer.innerHTML = "";
 });
